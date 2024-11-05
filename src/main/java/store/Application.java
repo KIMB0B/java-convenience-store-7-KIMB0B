3da4e25fd8090aaf665d8a6de8e933455a47eab5
KIMB0B <axzsd96301@gmail.com>
2024-11-05 23:32:45 +0900

    feat(OutputView): 상품 목록들을 가져와 출력하는 기능 추가

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,19 @@
 package store;
 
+import store.util.FileLoader;
+import store.util.Loader;
+import store.view.OutputView;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final String promotionFilePath = "src/main/resources/promotions.md";
+        final String productFilePath = "src/main/resources/products.md";
+
+        List<Promotion> promotions = Loader.loadPromitions(FileLoader.loadFile(promotionFilePath));
+        List<Product> products = Loader.loadProducts(FileLoader.loadFile(productFilePath), promotions);
+
+        OutputView.printProducts(products);
     }
 }
--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,9 +1,22 @@
 package store.view;
 
+import store.Product;
+import store.Promotion;
+
+import java.util.List;
+
 public class OutputView {
-    public void printProducts() {
-        System.out.println("- 콜라 1,000원 10개 탄산2+1");
-        // ...
+    public static void printProducts(List<Product> products) {
+        System.out.println("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n");
+        for (Product product : products) {
+            Promotion promotion = product.getPromotion();
+            System.out.printf("- %s %d %d ", product.getName(), product.getPrice(), product.getQuantity());
+            try {
+                System.out.println(promotion.getName());
+            } catch (NullPointerException e) {
+                System.out.println("null");
+            }
+        }
+        System.out.println();
     }
-    // ...
 }
