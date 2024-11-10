c4c4df7596a20ab8893635f59ab32e65d711c496
KIMB0B <axzsd96301@gmail.com>
2024-11-11 00:52:54 +0900

    refactor(view): InputView, OutputView 클래스 간단화 진행

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -6,50 +6,57 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class InputView {
+
+    private static final String ITEM_INPUT_PROMPT = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
+    private static final String MEMBERSHIP_PROMPT = "멤버십 할인을 받으시겠습니까?";
+    private static final String BUY_MORE_PROMPT = "현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까?";
+    private static final String ONE_MORE_PROMPT = "감사합니다. 구매하고 싶은 다른 상품이 있나요?";
+    private static final String CANT_PROMOTION_PROMPT = "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까?";
+
     public static Map<String, Integer> readItem() {
-        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+        System.out.println(ITEM_INPUT_PROMPT);
         String input = Console.readLine();
 
         Map<String, Integer> items = new LinkedHashMap<>();
+
         for (String item : input.split(",")) {
-            String[] itemInfo = item.split("-");
-            String itemName = itemInfo[0];
-            String itemQuantity = itemInfo[1];
-            if (itemName.startsWith("[") && itemQuantity.endsWith("]")) {
-                itemName = itemName.substring(1);
-                itemQuantity = itemQuantity.substring(0, itemQuantity.length() - 1);
-            }
-            items.put(itemName, Integer.parseInt(itemQuantity));
+            parseAndPutItems(items, item);
         }
         System.out.println();
         return items;
     }
 
     public static String readMembership() {
-        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
-        String input = Console.readLine().trim();
-        System.out.println();
-        return input;
+        return readYesNoInput(MEMBERSHIP_PROMPT);
     }
 
     public static String readBuyMore(String name, int count) {
-        System.out.printf("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n", name, count);
-        String input = Console.readLine().trim();
-        System.out.println();
-        return input;
+        return readYesNoInput(String.format(BUY_MORE_PROMPT, name, count));
     }
 
     public static String readOneMore() {
-        System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
+        return readYesNoInput(ONE_MORE_PROMPT);
+    }
+
+    public static String readCantPromotion(String name, int count) {
+        return readYesNoInput(String.format(CANT_PROMOTION_PROMPT, name, count));
+    }
+
+    private static String readYesNoInput(String prompt) {
+        System.out.println(prompt + " (Y/N)");
         String input = Console.readLine().trim();
         System.out.println();
         return input;
     }
 
-    public static String readCantPromotion(String name, int count) {
-        System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n", name, count);
-        String input = Console.readLine().trim();
-        System.out.println();
-        return input;
+    private static void parseAndPutItems(Map<String, Integer> items, String item) {
+        String[] itemInfo = item.split("-");
+        String itemName = itemInfo[0];
+        String itemQuantity = itemInfo[1];
+        if (itemName.startsWith("[") && itemQuantity.endsWith("]")) {
+            itemName = itemName.substring(1);
+            itemQuantity = itemQuantity.substring(0, itemQuantity.length() - 1);
+        }
+        items.put(itemName, Integer.parseInt(itemQuantity));
     }
 }
--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -2,7 +2,6 @@ package store.view;
 
 import store.Order;
 import store.Product;
-import store.Promotion;
 
 import java.util.List;
 
@@ -10,33 +9,47 @@ public class OutputView {
     public static void printProducts(List<Product> products) {
         System.out.println("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n");
         for (Product product : products) {
-            Promotion promotion = product.getPromotion();
-            String price = String.format("%,d", product.getPrice()) + "원";
-            String quantity = product.getQuantity() + "개";
-            if (product.getQuantity() == 0) {
-                quantity = "재고 없음";
-            }
-            System.out.printf("- %s %s %s ", product.getName(), price, quantity);
-            try {
-                System.out.println(promotion.getName());
-            } catch (NullPointerException e) {
-                System.out.println();
-            }
+            printProduct(product);
         }
         System.out.println();
     }
 
+    private static void printProduct(Product product) {
+        String quantity = product.getQuantity() + "개";
+        if (product.getQuantity() == 0) {
+            quantity = "재고 없음";
+        }
+        System.out.printf("- %s %s %s ", product.getName(), String.format("%,d", product.getPrice()) + "원", quantity);
+        try {
+            System.out.println(product.getPromotion().getName());
+        } catch (NullPointerException e) {
+            System.out.println();
+        }
+    }
+
     public static void printReceipt(List<Order> orders) {
+        printReceiptHeader(orders);
+        printFreeGift(orders);
+        printReceiptSummary(orders);
+    }
+
+    private static void printReceiptHeader(List<Order> orders) {
         System.out.println("===========W 편의점=============\n상품명\t\t수량\t\t금액");
         for (Order order : orders) {
             System.out.println(order.getNonePromotionProduct().getName() + "\t\t\t" + order.getQuantity() + "\t" + String.format("%,d", order.totalUseMoney()));
         }
-        System.out.println("===========증\t정=============");
+    }
+
+    private static void printFreeGift(List<Order> orders) {
+        System.out.println("============증정==============");
         for (Order order : orders) {
             if (order.countPromotionQuantity() > 0) {
                 System.out.println(order.getNonePromotionProduct().getName() + "\t\t\t" + order.countFreeItem());
             }
         }
+    }
+
+    private static void printReceiptSummary(List<Order> orders) {
         System.out.println("==============================");
         System.out.println("총구매액\t\t\t" + orders.stream().mapToInt(Order::getQuantity).sum() + "\t\t" + String.format("%,d", orders.stream().mapToInt(Order::totalUseMoney).sum()));
         System.out.println("행사할인\t\t\t\t\t-" + String.format("%,d", orders.stream().mapToInt(Order::promotionDiscount).sum()));
